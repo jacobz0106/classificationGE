ea7836d16f1b9d6af4ff63399a6ab502052ba11b
Jacob Zhu <jacobz0106@gmail.com>
2023-11-14 17:59:55 -0800

    upd

--- timeComp.sh
+++ timeComp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+module load python/3.9
 virtualenv --no-download ENV
 source ENV/bin/activate
-module load python/3.9
 pip install --no-index --upgrade pip
 pip install -r requirements.txt
 python timeComparisonPOF.py
