eb5d3970f6a3a21b3e92096612e0d57f3b1f660d
Jacob Zhu <jacobz0106@gmail.com>
2023-11-23 16:21:46 -0800

    upd

--- timeComparisonPOF.py
+++ timeComparisonPOF.py
@@ -15,7 +15,7 @@ def timeComparisonPOF(outfile, repeat = 20, N =[100,200,400,600,1000,1500], iniP
 		for j in range(repeat):
 			print([n,j])
 
-			matrix[i*len(N) + j,0] = n
+			matrix[i*repeat + j,0] = n
 
 			dataSIP_acceptreject = SIP_Data(integral_3D, DQ_Dlambda_3D, 3.75, len(domains) , *domains)
 			dataSIP_kdDart = SIP_Data(integral_3D, DQ_Dlambda_3D, 3.75, len(domains) , *domains)
@@ -24,19 +24,19 @@ def timeComparisonPOF(outfile, repeat = 20, N =[100,200,400,600,1000,1500], iniP
 			dataSIP_acceptreject.generate_POF(n = N[i], CONST_a = 1.5 ,iniPoints = iniPoints, sampleCriteria = 'accept-reject')
 			end_time = time.time()
 
-			matrix[i*len(N) + j,1] = end_time - start_time
+			matrix[i*repeat + j,1] = end_time - start_time
 
 			start_time = time.time()
 			dataSIP_kdDart.generate_POF(n = N[i], CONST_a = 1.5 ,iniPoints = iniPoints, sampleCriteria = 'k-dDarts')
 			end_time = time.time()
 
-			matrix[i*len(N) + j,2] = end_time - start_time
+			matrix[i*repeat + j,2] = end_time - start_time
 
 	df = pd.DataFrame(matrix, columns=['n','accept-reoject', 'k-dDarts'])
 	df.to_csv(outfile, index=False)
 
 def main():
-	timeComparisonPOF(outfile = 'Results/timeComparisonPofAcceptReject.py',repeat = 20, N =[100,200,300,400,500,600,700,800], iniPoints = 10)
+	timeComparisonPOF(outfile = 'Results/timeComparisonPofAcceptReject.csv',repeat = 20, N =[100,200,300,400,500,600,700,800], iniPoints = 10)
 
 
 
