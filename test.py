81dcf2476df72dba2a3bc06f8801fdda9eb6c556
Jacob Zhu <jacobz0106@gmail.com>
2023-11-25 16:18:37 -0800

    update

---
+++ test.py
@@ -0,0 +1,19 @@
+from dataGeneration import *
+import matplotlib.pyplot as plt
+from hierarchicalClustering import hierarchicalClustering
+from CBP import CBP
+from sklearn.preprocessing import StandardScaler
+from sklearn import cluster
+import cvxpy as cp
+
+
+
+
+def main():
+
+	print(cp.installed_solvers())
+
+
+
+if __name__ == '__main__':
+	main()
