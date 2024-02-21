e1066c08fe7198162a82f9f1a37e569f3e55068f
Jacob Zhu <jacobz0106@gmail.com>
2024-02-21 15:18:29 -0800

    rename

--- batchTask/bestModel_bruss.sh
+++ batchTask/bestModel_bruss.sh
@@ -3,4 +3,4 @@ module load gurobi/10.0.3 python/3.9
 source ~/env_gurobi/bin/activate
 pip install --no-index --upgrade pip
 pip install -r ../requirements.txt
-python ../best_model_gridsearchCV.py
+python ../best_model_gridsearchCV_bruss.py
