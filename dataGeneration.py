bf35586eaf2890bd7c50571154b918a5d82ab3c5
Jacob Zhu <jacobz0106@gmail.com>
2024-02-21 14:12:27 -0800

    update elliptic

--- dataGeneration.py
+++ dataGeneration.py
@@ -120,16 +120,22 @@ def DQ_Dlambda_3D(Lambda,T = 5,n = 100):
 
 #----------------------------------------- Elliptical -----------------------------------------
 
+# gradient part imcomplete
 
 class elliptic(object):
 	def __init__(self, n):
 		self.df = []
 		self.u = []
 		self.n = n
+		self.du_dlambda_1  = []
+		self.du_dlambda_2  = []
+		self.du_dlambda_3  = []
+		self.du_dlambda_4  = []
 
-	def function_y(self, Lambda, n):
+	def function_y(self, Lambda):
 		'''
 		'''
+		n = self.n
 		x_seq = np.arange(0,n+1)*1/n
 		A = np.zeros((n + 1, n + 1))
 		C = np.zeros(n + 1)
@@ -142,7 +148,7 @@ class elliptic(object):
 				A[i,i] = 1
 				C[i] = 0
 			else:
-				A[i, i-1 : i +2] =  self.coefficients(Lambda,x_seq[i],n)
+				A[i, i-1 : i +2] =  self.coefficients(Lambda,x_seq[i])
 				x = x_seq[i]
 				C[i] = (1 - x)*np.tanh(4*(x - Lambda[2])) + np.sin(5*np.pi*Lambda[3]*x)
 
@@ -153,85 +159,160 @@ class elliptic(object):
 	def mapping_Q(self,type_ = "mean"):
 		'''
 		'''
+
 		if type_ == "mean":
 			return np.sum(self.u*(1/self.n))
 
-	def Gradient_Q():
-		'''
-		'''
+
+	def dQ_dlambda_1(self,Lambda):
+
+		# check self.Lambda == Lambda
+		n = self.n
+		x_seq = np.arange(0,n+1)*1/n
+		A = np.zeros((n + 1, n + 1))
+		C = np.zeros(n + 1)
+
+		for i in range(n+1):
+			if i == 0:
+				A[i,0] = 1
+				C[i] = 0
+			elif i == n:
+				A[i,i] = 1
+				C[i] = 0
+			else:
+				A[i, i-1 : i +2] =  self.coefficients(Lambda,x_seq[i])
+				x = x_seq[i]
+				C[i] =  - np.sum([i*j for i, j in zip(self.coefficients_dlambda_1(Lambda,x_seq[i]), self.u[i-1 : i +2])])
+
+		# A*x = C
+		self.du_dlambda_1 = solve(A, C)
+
+	def dQ_dlambda_2(self,Lambda):
+
+		# check self.Lambda == Lambda
+		n = self.n
+		x_seq = np.arange(0,n+1)*1/n
+		A = np.zeros((n + 1, n + 1))
+		C = np.zeros(n + 1)
+
+		for i in range(n+1):
+			if i == 0:
+				A[i,0] = 1
+				C[i] = 0
+			elif i == n:
+				A[i,i] = 1
+				C[i] = 0
+			else:
+				A[i, i-1 : i +2] =  self.coefficients(Lambda,x_seq[i])
+				x = x_seq[i]
+				C[i] =  - np.sum([i*j for i, j in zip(self.coefficients_dlambda_2(Lambda,x_seq[i]), self.u[i-1 : i +2])])
+
+		# A*x = C
+		self.du_dlambda_2 = solve(A, C)
+
+	def dQ_dlambda_3(self,Lambda):
+
+		# check self.Lambda == Lambda
+		n = self.n
+		x_seq = np.arange(0,n+1)*1/n
+		A = np.zeros((n + 1, n + 1))
+		C = np.zeros(n + 1)
+
+		for i in range(n+1):
+			if i == 0:
+				A[i,0] = 1
+				C[i] = 0
+			elif i == n:
+				A[i,i] = 1
+				C[i] = 0
+			else:
+				A[i, i-1 : i +2] =  self.coefficients(Lambda,x_seq[i])
+				x = x_seq[i]
+				C[i] =  (1 - x)*(1 - np.tanh(4*(x - Lambda[2]))**2)*(-4)
+
+		# A*x = C
+		self.du_dlambda_3 = solve(A, C)
+
+	def dQ_dlambda_4(self,Lambda):
+
+		# check self.Lambda == Lambda
+		n = self.n
+		x_seq = np.arange(0,n+1)*1/n
+		A = np.zeros((n + 1, n + 1))
+		C = np.zeros(n + 1)
+
+		for i in range(n+1):
+			if i == 0:
+				A[i,0] = 1
+				C[i] = 0
+			elif i == n:
+				A[i,i] = 1
+				C[i] = 0
+			else:
+				A[i, i-1 : i +2] =  self.coefficients(Lambda,x_seq[i])
+				x = x_seq[i]
+				C[i] =  np.cos(5*np.pi*Lambda[3]*x)*(5*np.pi*x)
+
+		# A*x = C
+		self.du_dlambda_4 = solve(A, C)
 
 
-	def coefficients(self,Lambda, x,n):
+
+	def Gradient_Q(self,Lambda, type_ = "mean"):
+		'''
+		'''
+		self.function_y(Lambda)
+		self.dQ_dlambda_1(Lambda)
+		self.dQ_dlambda_2(Lambda)
+		self.dQ_dlambda_3(Lambda)
+		self.dQ_dlambda_4(Lambda)
+
+		if type_ == "mean":
+			return([np.sum(self.du_dlambda_1*(1/self.n)), np.sum(self.du_dlambda_2*(1/self.n)), np.sum(self.du_dlambda_3*(1/self.n)), np.sum(self.du_dlambda_4*(1/self.n))] )
+
+	def coefficients(self,Lambda, x):
 		'''
 		return coefficient for u(x-h), u(x), u(x+h)
 		'''
-		h = 1/n
-		a = -(2*x*np.exp(-Lambda[0]*x) - Lambda[0]*x**2*np.exp(-Lambda[0]*x) )
+		h = 1/self.n
+		a = -(2*x*np.exp(-Lambda[0]*x) - Lambda[0]*x**2*np.exp(-Lambda[0]*x) - Lambda[1])
 		b = -(x**2*np.exp( -Lambda[0]*x ) + 0.05 )
 
-		return [ b/h**2, -((a+Lambda[1])/h - 2*b/h**2 ), (a + Lambda[1])/h + b/h**2 ]
+		return [ b/h**2 - a/h, a/h - 2*b/h**2, b/h**2]
+
+	def coefficients_dlambda_1(self,Lambda,x):
+		'''
+		return coefficient for u(x-h), u(x), u(x+h) as defined in appendex B
+		'''
+		h = 1/self.n
+		a = -(2*x*np.exp(-Lambda[0]*x) - Lambda[0]*x**2*np.exp(-Lambda[0]*x) - Lambda[1])
+		b = -(x**2*np.exp( -Lambda[0]*x ) + 0.05 )
+		da_dlambda_1 = 3*x**2*np.exp(-Lambda[0]*x) - Lambda[0]*x**3*np.exp(-Lambda[0]*x)
+		db_dlambda_1 = -x**3*np.exp(-Lambda[0]*x)
+
+		return [ db_dlambda_1/h**2 - da_dlambda_1/h, da_dlambda_1 /h - 2*db_dlambda_1/h**2, db_dlambda_1/h**2]
+
+	def coefficients_dlambda_2(self,Lambda,x):
+		h = 1/self.n
+		da_dlambda_2 = 1
+		db_dlambda_2 = 0
+		return [ db_dlambda_2/h**2 - da_dlambda_2/h, da_dlambda_2 /h - 2*db_dlambda_2/h**2, db_dlambda_2/h**2]
+
+	# lamda 3/4 has 0 coefficients
 
 
+def elliptic_function(Lambda, n):
+	elliptic_gen = elliptic(n)
+	elliptic_gen.function_y(Lambda)
+	Q = elliptic_gen.mapping_Q()
+	del elliptic_gen
+	return Q
 
-
-
-
-
-
-
-
-
-
-#----------------------------------------- Data_Generation-----------------------------------------
-
-class SIP_Data(object):
-	def __init__(self, function_y, function_gradient,CONST_threshold, dim, *domain):
-		if len(domain) != dim:
-			raise ValueError(f"Expected {dim} domain intervals, but got {len(domain)}")
-		
-		# Check if all arguments are tuples with two elements
-		for arg in domain:
-			if len(arg) != 2:
-				raise TypeError("Each domain argument must be a tuple of two numbers (low, high)")
-
-		self.CONST_threshold = CONST_threshold
-		self.dim = dim
-		self.domain = domain
-		self.function_y = function_y
-		self.function_gradient = function_gradient
-		self.df = []
-		self.Gradient = []
-
-	def generate_Uniform(self,n):
-		points = np.array([np.random.uniform(low, high, n) for low, high in self.domain]).T
-
-		self.df = pd.DataFrame(points, columns = [f'X{i+1}' for i in range(self.dim)])
-
-		Z = self.df.apply(self.function_y, axis = 1)
-		self.Gradient = self.df.apply(self.function_gradient, axis = 1)
-		self.df['Label'] = np.zeros(n) -1
-		index = Z >= self.CONST_threshold 
-		self.df['Label'][index] = 1
-		self.df['f'] = Z
-		
-
-	def generate_POF(self,n,CONST_a ,iniPoints = 0, max_iterations  = 1000, max_miss = 1000,sampleCriteria = 'k-dDarts', Initialization = True):
-		if Initialization == True:
-			self.POFdarts = POFdarts( self.function_y, self.function_gradient , CONST_a,  self.CONST_threshold , max_iterations  = max_iterations, max_miss = max_miss )
-			self.POFdarts.Initialize(iniPoints, self.dim , self.domain)
-		self.POFdarts.Generate_data( n - iniPoints, self.dim , self.domain, sampleCriteria = sampleCriteria)
-		self.Gradient = self.POFdarts.Q
-
-		#self.df = pd.DataFrame(data={"X":np.array(self.POFdarts.df)[:,0],"Y":np.array(self.POFdarts.df)[:,1]})
-		self.df = pd.DataFrame(np.array(self.POFdarts.df), columns = [f'X{i+1}' for i in range(self.dim)])
-
-		self.df['Label'] = np.zeros( len(self.POFdarts.df) ) -1
-		index = np.array(self.POFdarts.y) >= self.CONST_threshold 
-		self.df['Label'][index] = 1
-		self.df['f'] = self.POFdarts.y
-
-
-
+def elliptic_Gradient(Lambda, n):
+	elliptic_gen = elliptic(100)
+	dQ = elliptic_gen.Gradient_Q(lambda_)
+	del elliptic_gen
+	return dQ
 
 
 
@@ -243,7 +324,9 @@ class SIP_Data(object):
 def function1(x):
 	return ((x[1]-.5*(np.tanh(20*x[0])*np.tanh(20*(x[0]-.5))+1)*np.exp(.2*x[0]**2)))**2
 
-def Gradient_f1(x, y):
+def Gradient_f1(X):
+	x = X[0]
+	y = X[1]
 	dx = 2*((y-.5*(np.tanh(20*x)*np.tanh(20*(x-.5))+1)*np.exp(.2*x**2)))*( 
 		-0.5*(
 				(0.4*x)*np.exp(0.2*x**2)*(np.tanh(20*x)*np.tanh(20*(x-.5)) + 1 )+
@@ -256,49 +339,21 @@ def Gradient_f1(x, y):
 
 
 
-
-# def example1_Data(n, c = 0.5): 
-#     X = np.random.uniform(0, 2, n)
-#     Y = np.random.uniform(0, 2, n)
-#     df=pd.DataFrame(data={"X":X,"Y":Y})
-#     Z = df.apply(function1, axis = 1)
-#     df['Z'] = np.zeros(n) - 1
-#     index = Z >= c
-#     df['Z'][index] = 1 
-#     return df
-
-# def example1_with_f_POF(numPoints, CONST_a, CONST_threshold = 0.5, iniPoints = 1):
-
-# 	POF = POFdarts(function1, Gradient_f1, CONST_a, CONST_threshold)
-# 	POF.Generate_2D(iniPoints = iniPoints, N = numPoints - iniPoints, xlim = [0,2], ylim = [0,2])
-	
-# 	L = np.zeros(numPoints) - 1
-# 	L[np.array(POF.y) <= CONST_threshold] = 1
-# 	data = {
-# 		'X': np.array(POF.df)[:,0],
-# 		'Y': np.array(POF.df)[:,1],
-# 		'Z': np.array(POF.y),
-# 		'Label': L
-# 	}
-# 	df = pd.DataFrame(data)
-# 	return df
-
 # Simulated example 2 -------------------------
 
-B = 1
-A = 10
-def function2(x, A = 10, B = 1):
-	return 1 + np.tanh(B*(x[1] - A*x[0]*(x[0] - 1/2)*(x[0]+1/2)))
+def function2(X, A = 10, B = 1):
+	x = X[0]
+	y = X[1]
+	return 1 + np.tanh(B*(y - A*x*(x - 1/2)*(x+1/2)))
 
-# def example2_Data(n, c = 0.5): 
-#     X = np.random.uniform(-1, 1, n)
-#     Y = np.random.uniform(-1, 1, n)
-#     df=pd.DataFrame(data={"X":X,"Y":Y})
-#     Z = df.apply(function2, axis = 1)
-#     df['Z'] = np.zeros(n) - 1
-#     index = Z >= c
-#     df['Z'][index] = 1 
-#     return df
+def Gradient_f2(X, A = 10, B = 1):
+	x = X[0]
+	y = X[1]
+	dx = (1 -np.tanh(B*(y - A*x*(x - 1/2)*(x+1/2)))**2 )*(3*A*x**2 - 1/4*A)
+	dy = (1 -np.tanh(B*(y - A*x*(x - 1/2)*(x+1/2)))**2 )*B
+	return np.array([dx,dy]) 
+
+
 
 
 
@@ -416,9 +471,57 @@ def create_binary_class_boundary_spiral(num_points):
 	df = pd.DataFrame(data)
 	return df
 
+#------------------------------------------------------------------------------------------------------
+#----------------------------------------- Data_Generation---------------------------------------------
 
-#--------------------------------------------------------Unbalanced data----------------------------------------
+class SIP_Data(object):
+	'''
+	function_y and function_gradient takes 1 vector argument 
+	'''
+	def __init__(self, function_y, function_gradient,CONST_threshold, dim, *domain):
+		if len(domain) != dim:
+			raise ValueError(f"Expected {dim} domain intervals, but got {len(domain)}")
+		
+		# Check if all arguments are tuples with two elements
+		for arg in domain:
+			if len(arg) != 2:
+				raise TypeError("Each domain argument must be a tuple of two numbers (low, high)")
 
+		self.CONST_threshold = CONST_threshold
+		self.dim = dim
+		self.domain = domain
+		self.function_y = function_y
+		self.function_gradient = function_gradient
+		self.df = []
+		self.Gradient = []
+
+	def generate_Uniform(self,n):
+		points = np.array([np.random.uniform(low, high, n) for low, high in self.domain]).T
+
+		self.df = pd.DataFrame(points, columns = [f'X{i+1}' for i in range(self.dim)])
+
+		Z = self.df.apply(self.function_y, axis = 1)
+		self.Gradient = self.df.apply(self.function_gradient, axis = 1)
+		self.df['Label'] = np.zeros(n) -1
+		index = Z >= self.CONST_threshold 
+		self.df['Label'][index] = 1
+		self.df['f'] = Z
+		
+
+	def generate_POF(self,n,CONST_a ,iniPoints = 0, max_iterations  = 1000, max_miss = 1000,sampleCriteria = 'k-dDarts', Initialization = True):
+		if Initialization == True:
+			self.POFdarts = POFdarts( self.function_y, self.function_gradient , CONST_a,  self.CONST_threshold , max_iterations  = max_iterations, max_miss = max_miss )
+			self.POFdarts.Initialize(iniPoints, self.dim , self.domain)
+		self.POFdarts.Generate_data( n - iniPoints, self.dim , self.domain, sampleCriteria = sampleCriteria)
+		self.Gradient = self.POFdarts.Q
+
+		#self.df = pd.DataFrame(data={"X":np.array(self.POFdarts.df)[:,0],"Y":np.array(self.POFdarts.df)[:,1]})
+		self.df = pd.DataFrame(np.array(self.POFdarts.df), columns = [f'X{i+1}' for i in range(self.dim)])
+
+		self.df['Label'] = np.zeros( len(self.POFdarts.df) ) -1
+		index = np.array(self.POFdarts.y) >= self.CONST_threshold 
+		self.df['Label'][index] = 1
+		self.df['f'] = self.POFdarts.y
 
 
 
