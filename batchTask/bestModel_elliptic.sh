d679a8f0e8ceaf9863052d61ae63cafb75e4f8d8
Jacob Zhu <jacobz0106@gmail.com>
2024-02-26 14:55:25 -0800

    upd

--- batchTask/bestModel_elliptic.sh
+++ batchTask/bestModel_elliptic.sh
@@ -3,5 +3,5 @@ module load gurobi/10.0.3 python/3.9
 source ~/env_gurobi/bin/activate
 pip install --no-index --upgrade pip
 pip install -r ../requirements.txt
-#export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=1
 python ../best_model_gridsearchCV_elliptic.py
