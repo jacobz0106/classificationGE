fe82a77cc863e64f8d9a386524709f6f18a744f4
Jacob Zhu <jacobz0106@gmail.com>
2024-02-27 20:13:02 -0800

    upd

--- batchTask/sbatch_100CV.sh
+++ batchTask/sbatch_100CV.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sbatch --time=124:00:00 -o out/brussoutput.out -e out/bruss.err bestModel_bruss.sh 
-sbatch --time=124:00:00 -o out/ellipticoutput.out -e out/elliptic.err bestModel_elliptic.sh 
-sbatch --time=124:00:00 -o out/f1output.out -e out/f1.err bestModel_f1.sh 
-sbatch --time=124:00:00 -o out/f2output.out -e out/f2.err bestModel_f2.sh 
+sbatch --time=124:00:00 --cpus-per-task=4 --mem-per-cpu=2G -o out/brussoutput.out -e out/bruss.err bestModel_bruss.sh 
+sbatch --time=124:00:00 --cpus-per-task=4 --mem-per-cpu=2G -o out/ellipticoutput.out -e out/elliptic.err bestModel_elliptic.sh 
+sbatch --time=124:00:00 --cpus-per-task=4 --mem-per-cpu=2G -o out/f1output.out -e out/f1.err bestModel_f1.sh 
+sbatch --time=124:00:00 --cpus-per-task=4 --mem-per-cpu=2G -o out/f2output.out -e out/f2.err bestModel_f2.sh 
