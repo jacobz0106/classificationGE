770eb977255ea952687df60ff59e22b3639a3710
Jacob Zhu <jacobz0106@gmail.com>
2024-02-28 16:37:46 -0800

    upd

--- batchTask/gurobi.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-module load gurobi/10.0.3 python/3.9
-source ~/env_gurobi/bin/activate
-pip install --no-index --upgrade pip
-pip install -r ../requirements.txt
-export OMP_NUM_THREADS=10
-export GRB_TOKENSERVER=license1.computecanada.ca
-echo "installation of req packages done."
-python ../gurobiExample.py 100 500 Brusselator POF
--- batchTask/gurobitest.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-module load gurobi/10.0.3 python/3.9
-source ~/env_gurobi/bin/activate
-pip install --no-index --upgrade pip
-pip install -r ../requirements.txt
-export OMP_NUM_THREADS=10
-python ../gurobiTest.py
