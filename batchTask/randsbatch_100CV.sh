b11e658c88db91ab430b60c6385c4472f6222b7e
Jacob Zhu <jacobz0106@gmail.com>
2024-02-28 16:34:58 -0800

    upd

--- batchTask/randsbatch_100CV.sh
+++ batchTask/randsbatch_100CV.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sbatch --time=124:00:00 --cpus-per-task=4 --mem-per-cpu=2G -o out/brussoutput.out -e out/bruss.err randbestModel_bruss.sh 
-sbatch --time=124:00:00 --cpus-per-task=4 --mem-per-cpu=2G -o out/ellipticoutput.out -e out/elliptic.err randbestModel_elliptic.sh 
-sbatch --time=124:00:00 --cpus-per-task=4 --mem-per-cpu=2G -o out/f1output.out -e out/f1.err randbestModel_f1.sh 
-sbatch --time=124:00:00 --cpus-per-task=4 --mem-per-cpu=2G -o out/f2output.out -e out/f2.err randbestModel_f2.sh 
+sbatch --time=124:00:00 --cpus-per-task=4 --mem-per-cpu=2G -o out/brussoutputRand.out -e out/brussRand.err randbestModel_bruss.sh 
+sbatch --time=124:00:00 --cpus-per-task=4 --mem-per-cpu=2G -o out/ellipticoutputRand.out -e out/ellipticRand.err randbestModel_elliptic.sh 
+sbatch --time=124:00:00 --cpus-per-task=4 --mem-per-cpu=2G -o out/f1outputRand.out -e out/f1Rand.err randbestModel_f1.sh 
+sbatch --time=124:00:00 --cpus-per-task=4 --mem-per-cpu=2G -o out/f2outputRand.out -e out/f2Rand.err randbestModel_f2.sh 
