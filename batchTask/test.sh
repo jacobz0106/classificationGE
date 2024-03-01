6a8791bf0efd6da5431a0cd08cd4b454ea17f379
Jacob Zhu <jacobz0106@gmail.com>
2024-03-01 00:06:50 -0800

    upd

--- batchTask/test.sh
+++ batchTask/test.sh
@@ -3,7 +3,6 @@ module load gurobi/10.0.3 python/3.9
 source ~/env_gurobi/bin/activate
 pip install --no-index --upgrade pip
 pip install -r ../requirements.txt
-export OMP_NUM_THREADS=1
 export GRB_TOKENSERVER=license1.computecanada.ca
 echo "installation of req packages done."
 python ../testBest_model.py 100 500 Function1 POF
