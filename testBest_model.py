9ada83c1e5a3b516c6281bfa04c76efa6c16e3b6
Jacob Zhu <jacobz0106@gmail.com>
2024-02-29 23:50:51 -0800

    upd

--- testBest_model.py
+++ testBest_model.py
@@ -200,54 +200,54 @@ def Accuracy_comparison_CV(n , nTest, example, sample_crite = 'POF', repeat = 20
     X_test = dataSIP.df.iloc[:, :-2].values
     y_test = dataSIP.df['Label'].values
 
-    for model, para, k in zip(Classifier, paras, range(len(Classifier))):
-      print('-------------------------------------------------')
-      print('Tunning model:' + str(model)+ '\n')
-      print('with parameters:' +  str(para) + '\n')
+    # for model, para, k in zip(Classifier, paras, range(len(Classifier))):
+    #   print('-------------------------------------------------')
+    #   print('Tunning model:' + str(model)+ '\n')
+    #   print('with parameters:' +  str(para) + '\n')
 
-      # encode for Xgboost
-      if isinstance(model, XGBClassifier):
-        xgb_y_train = [0 if label == -1 else 1 for label in y_train]
-        xgb_y_test= [0 if label == -1 else 1 for label in y_test]
-        best_model = perform_grid_search_cv(model, para, X_train,xgb_y_train)
+    #   # encode for Xgboost
+    #   if isinstance(model, XGBClassifier):
+    #     xgb_y_train = [0 if label == -1 else 1 for label in y_train]
+    #     xgb_y_test= [0 if label == -1 else 1 for label in y_test]
+    #     best_model = perform_grid_search_cv(model, para, X_train,xgb_y_train)
         
-        trainAccuracy = np.sum(best_model.predict(X_train) == xgb_y_train)/len(xgb_y_train)
-        predictionAccuracy = np.sum(best_model.predict(X_test) == xgb_y_test)/len(xgb_y_test)
-        accuracyMatrixTrain[i, k] =  trainAccuracy
-        accuracyMatrixPrediction[i, k] = predictionAccuracy
-        print('training samples, best model has train accuracy: %f' %trainAccuracy + ' prediction accuracy:%f' %predictionAccuracy + '\n')
-      elif isinstance(model, GPSVM):
-        if model.method == "hierarchicalClustering":
-          # Create a GridSearchCV object
-          fit_para = {'dQ':dQ}
-          grid_search = GridSearchCV(model, para, cv=5, scoring='accuracy')
+    #     trainAccuracy = np.sum(best_model.predict(X_train) == xgb_y_train)/len(xgb_y_train)
+    #     predictionAccuracy = np.sum(best_model.predict(X_test) == xgb_y_test)/len(xgb_y_test)
+    #     accuracyMatrixTrain[i, k] =  trainAccuracy
+    #     accuracyMatrixPrediction[i, k] = predictionAccuracy
+    #     print('training samples, best model has train accuracy: %f' %trainAccuracy + ' prediction accuracy:%f' %predictionAccuracy + '\n')
+    #   elif isinstance(model, GPSVM):
+    #     if model.method == "hierarchicalClustering":
+    #       # Create a GridSearchCV object
+    #       fit_para = {'dQ':dQ}
+    #       grid_search = GridSearchCV(model, para, cv=5, scoring='accuracy')
 
-          # Fit the grid search to the data
-          grid_search.fit(X_train, y_train, **fit_para)
+    #       # Fit the grid search to the data
+    #       grid_search.fit(X_train, y_train, **fit_para)
 
-          # Get the best model with tuned hyperparameters
-          best_model = grid_search.best_estimator_
+    #       # Get the best model with tuned hyperparameters
+    #       best_model = grid_search.best_estimator_
 
-          trainAccuracy = np.sum(best_model.predict(X_train) == y_train)/len(y_train)
-          predictionAccuracy = np.sum(best_model.predict(X_test) == y_test)/len(y_test)
-          accuracyMatrixTrain[i, k] =  trainAccuracy
-          accuracyMatrixPrediction[i, k] = predictionAccuracy
-          print('training samples, best model has train accuracy: %f' %trainAccuracy + ' prediction accuracy:%f' %predictionAccuracy + '\n')
-        else:
-          model = GPSVM(method = "KMeans")
-          best_model = perform_grid_search_cv(model, para, X_train,y_train)
-          trainAccuracy = np.sum(best_model.predict(X_train) == y_train)/len(y_train)
-          predictionAccuracy = np.sum(best_model.predict(X_test) == y_test)/len(y_test)
-          accuracyMatrixTrain[i, k] =  trainAccuracy
-          accuracyMatrixPrediction[i, k] = predictionAccuracy
-          print('training samples, best model has train accuracy: %f' %trainAccuracy + ' prediction accuracy:%f' %predictionAccuracy + '\n')
-      else:
-        best_model = perform_grid_search_cv(model, para, X_train,y_train)
-        trainAccuracy = np.sum(best_model.predict(X_train) == y_train)/len(y_train)
-        predictionAccuracy = np.sum(best_model.predict(X_test) == y_test)/len(y_test)
-        accuracyMatrixTrain[i, k] =  trainAccuracy
-        accuracyMatrixPrediction[i, k] = predictionAccuracy
-        print('training samples, best model has train accuracy: %f' %trainAccuracy + ' prediction accuracy:%f' %predictionAccuracy + '\n')
+    #       trainAccuracy = np.sum(best_model.predict(X_train) == y_train)/len(y_train)
+    #       predictionAccuracy = np.sum(best_model.predict(X_test) == y_test)/len(y_test)
+    #       accuracyMatrixTrain[i, k] =  trainAccuracy
+    #       accuracyMatrixPrediction[i, k] = predictionAccuracy
+    #       print('training samples, best model has train accuracy: %f' %trainAccuracy + ' prediction accuracy:%f' %predictionAccuracy + '\n')
+    #     else:
+    #       model = GPSVM(method = "KMeans")
+    #       best_model = perform_grid_search_cv(model, para, X_train,y_train)
+    #       trainAccuracy = np.sum(best_model.predict(X_train) == y_train)/len(y_train)
+    #       predictionAccuracy = np.sum(best_model.predict(X_test) == y_test)/len(y_test)
+    #       accuracyMatrixTrain[i, k] =  trainAccuracy
+    #       accuracyMatrixPrediction[i, k] = predictionAccuracy
+    #       print('training samples, best model has train accuracy: %f' %trainAccuracy + ' prediction accuracy:%f' %predictionAccuracy + '\n')
+    #   else:
+    #     best_model = perform_grid_search_cv(model, para, X_train,y_train)
+    #     trainAccuracy = np.sum(best_model.predict(X_train) == y_train)/len(y_train)
+    #     predictionAccuracy = np.sum(best_model.predict(X_test) == y_test)/len(y_test)
+    #     accuracyMatrixTrain[i, k] =  trainAccuracy
+    #     accuracyMatrixPrediction[i, k] = predictionAccuracy
+    #     print('training samples, best model has train accuracy: %f' %trainAccuracy + ' prediction accuracy:%f' %predictionAccuracy + '\n')
   return accuracyMatrixTrain, accuracyMatrixPrediction
 
 
