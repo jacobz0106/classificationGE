3cb4165aa6e367505d8863d224eb0a82c97e90b4
Jacob Zhu <jacobz0106@gmail.com>
2024-03-05 11:10:21 -0800

    upd

--- batchTask/randsbatch_100CV.sh
+++ batchTask/randsbatch_100CV.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sbatch --time=124:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/brussoutputRand.out -e out/brussRand.err randbestModel_bruss.sh 
-sbatch --time=124:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/ellipticoutputRand.out -e out/ellipticRand.err randbestModel_elliptic.sh 
-sbatch --time=124:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/f1outputRand.out -e out/f1Rand.err randbestModel_f1.sh 
-sbatch --time=124:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/f2outputRand.out -e out/f2Rand.err randbestModel_f2.sh 
+sbatch --time=167:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/brussoutputRand.out -e out/brussRand.err randbestModel_bruss.sh 
+sbatch --time=167:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/ellipticoutputRand.out -e out/ellipticRand.err randbestModel_elliptic.sh 
+sbatch --time=167:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/f1outputRand.out -e out/f1Rand.err randbestModel_f1.sh 
+sbatch --time=167:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/f2outputRand.out -e out/f2Rand.err randbestModel_f2.sh 
--- batchTask/sbatch_100CV.sh
+++ batchTask/sbatch_100CV.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sbatch --time=124:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/brussoutput.out -e out/bruss.err bestModel_bruss.sh 
-sbatch --time=124:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/ellipticoutput.out -e out/elliptic.err bestModel_elliptic.sh 
-sbatch --time=124:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/f1output.out -e out/f1.err bestModel_f1.sh 
-sbatch --time=124:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/f2output.out -e out/f2.err bestModel_f2.sh 
+sbatch --time=167:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/brussoutput.out -e out/bruss.err bestModel_bruss.sh 
+sbatch --time=167:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/ellipticoutput.out -e out/elliptic.err bestModel_elliptic.sh 
+sbatch --time=167:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/f1output.out -e out/f1.err bestModel_f1.sh 
+sbatch --time=167:00:00 --cpus-per-task=2 --mem-per-cpu=1G -o out/f2output.out -e out/f2.err bestModel_f2.sh 
